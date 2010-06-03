c28b127056db94570424587bf55b9a6240f1f710
t.felgentreff <t.felgentreff@7926756e-e54e-46e6-9721-ed318f58905e>
2010-06-03 16:23:22 +0000

    move code into acts_as_journalized
    git-svn-id: https://dev.finnlabs.de/svn/cockpit/trunk/acts_as_journalized@1177 7926756e-e54e-46e6-9721-ed318f58905e

--- lib/acts_as_journalized.rb
+++ lib/acts_as_journalized.rb
@@ -60,6 +60,32 @@ module Redmine
           @current_journal
         end
         
+        # Saves the changes in a Journal
+        # Called after_save
+        def create_journal
+          if @current_journal
+            # attributes changes
+            (Issue.column_names - %w(id description lock_version created_on updated_on)).each {|c|
+              @current_journal.details << JournalDetail.new(:property => 'attr',
+              :prop_key => c,
+              :old_value => @object_before_change.send(c),
+              :value => send(c)) unless send(c)==@object_before_change.send(c)
+            }
+            if self.respond_to? :custom_values
+              # custom fields changes
+              custom_values.each {|c|
+                next if (@custom_values_before_change[c.custom_field_id]==c.value ||
+                (@custom_values_before_change[c.custom_field_id].blank? && c.value.blank?))
+                @current_journal.details << JournalDetail.new(:property => 'cf', 
+                :prop_key => c.custom_field_id,
+                :old_value => @custom_values_before_change[c.custom_field_id],
+                :value => c.value)
+              }
+            end
+            @current_journal.save
+          end
+        end
+        
         
         module ClassMethods
         end
