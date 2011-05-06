6c680e3f458c0ba052a154e1e86377ef90a4e25f
Eric Davis <edavis@littlestreamsoftware.com>
2011-05-06 14:01:38 -0700

    Migrate journals with custom values so they have a prefix

--- db/migrate/20100714111651_generalize_journals.rb
+++ db/migrate/20100714111651_generalize_journals.rb
@@ -36,7 +36,11 @@ class GeneralizeJournals < ActiveRecord::Migration
     JournalDetails.all.each do |detail|
       journal = Journal.find(detail.journal_id)
       changes = journal.changes || {}
-      changes[detail.prop_key.to_s] = [detail.old_value, detail.value]
+      if detail.property == 'attr' # Standard attributes
+        changes[detail.prop_key.to_s] = [detail.old_value, detail.value]
+      elsif detail.property == 'cf' # Custom fields
+        changes["custom_values_" + detail.prop_key.to_s] = [detail.old_value, detail.value]
+      end
       journal.update_attribute(:changes, changes.to_yaml)
     end
 
